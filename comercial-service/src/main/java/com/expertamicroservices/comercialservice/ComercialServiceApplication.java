78b7a48b36de28aa8b25c2580364ca8f35108a3a
Ignacio de Villafañe <idevillafane@gmail.com>
2022-12-28 16:57:12 -0300

    Fallo en config

--- comercial-service/src/main/java/com/expertamicroservices/comercialservice/ComercialServiceApplication.java
+++ comercial-service/src/main/java/com/expertamicroservices/comercialservice/ComercialServiceApplication.java
@@ -2,8 +2,10 @@ package com.expertamicroservices.comercialservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ComercialServiceApplication {
 
 	public static void main(String[] args) {
